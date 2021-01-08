0cae697fc37a54e42b0a0979caf6fa0576030435
Nasrul Fuad <nasrulfuad08@gmail.com>
2021-01-08 19:34:21 +0700

    Updates

--- src/events/index.ts
+++ src/events/index.ts
@@ -5,8 +5,12 @@ export * from "./types/Subjects";
 
 export * from "./TicketCreatedEvent";
 export * from "./TicketUpdatedEvent";
+
 export * from "./OrderCancelledEvent";
 export * from "./OrderCreatedEvent";
+
 export * from "./ExpirationCompleteEvent";
+
+export * from "./PaymentCreatedEvent";
 
 export * from "./types/OrderStatus";
